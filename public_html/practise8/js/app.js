48d7efa4f9ce9201eec380972e6662d3d58e821a
ПК <@DESKTOP-K8N965B>
2020-11-02 17:31:39 +0200

    First commit

---
+++ public_html/practise8/js/app.js
@@ -0,0 +1,55 @@
+$(document).ready( () => {
+
+	const trigger = $('#trigger'),
+	      emailPlace = $('#emailAddress'),
+		  namePlace = $('#trigger'),
+	 	  avatarPlace = $('#avatar')
+
+	let counter = 1
+
+	trigger.bind('click', () => {
+		const xhr = new XMLHttpRequest()
+		let url = `https://webdesognfor.herokuapp.com/users/${counter}`
+
+		counter++
+
+		xhr.onload = function () {
+			if(this.status === 200) {
+				try {
+					let data = JSON.parse(this.responseText).data,
+						email = data.email,
+						firstName = data.first_name,
+						lastName = data.last_name,
+						avatar = data.avatar,
+						myEmail = 'bodianych2020@gmail.com',
+						myFirstName = 'Bohdan',
+						myLastName = 'Khomik',
+						myAvatar = 'CvImage/photo5418313102212051072.jpg'
+
+					if ( ((counter-1)  % 5 ) === 0 ) {
+						emailPlace.html(myEmail)
+						namePlace.html(`${myFirstName} <br> ${myLastName}`)
+						avatarPlace.attr('src', myAvatar)
+						
+					} else {
+						emailPlace.html(email)
+						namePlace.html(`${firstName} <br> ${lastName}`)
+						avatarPlace.attr('src', avatar)
+					}
+
+				} catch (e) {
+					console.warn('JSON is broken')
+				}
+	
+			}else {
+				console.warn('No accessable site...')
+			}
+		}
+
+		xhr.open('get', url)
+		xhr.send()
+
+		counter = (counter > 12)? 1 : counter
+	})
+})
+
